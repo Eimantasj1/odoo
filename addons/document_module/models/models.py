38c10f3c405378e5a9944087defb0de71111adce
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-12-08 17:16:45 +0200

    models.py ir views.xml

--- addons/document_module/models/models.py
+++ addons/document_module/models/models.py
@@ -10,3 +10,12 @@ class Document(models.Model):
     creator_user_id = fields.Many2one('res.users', string='Created By', default=lambda self: self.env.user)
     responsible_employee_ids = fields.Many2many('res.users', string='Responsible Employees')
     created_date = fields.Date(string='Created Date', default=fields.Date.today)
+
+    # Action
+    action_view = {
+        'name': 'Document Tree View',
+        'type': 'ir.actions.act_window',
+        'view_mode': 'tree,form',
+        'res_model': 'document_module.document',
+        'view_id': False,
+    }
